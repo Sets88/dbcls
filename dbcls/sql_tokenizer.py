d7edb4f38f1dba9aae40322a81722e96b222c113
Maxim Nikitenko <mnikitenko@expertoption.com>
2023-08-28 22:06:09 +0300

    case insensitive SQL keywords

--- dbcls/sql_tokenizer.py
+++ dbcls/sql_tokenizer.py
@@ -1,4 +1,5 @@
 from kaa.theme import Style
+from kaa import doc_re
 from kaa.syntax_highlight import Span, SingleToken, Tokenizer, Keywords, Token
 
 
@@ -8,6 +9,22 @@ sql_editor_themes = {
         Style('number', 'Yellow', None, bold=True),
     ]
 }
+
+class CaseInsensitiveKeywords(Keywords):
+    def re_start(self) -> str:
+        tokens = []
+        for token in self._tokens:
+            new_token = ''
+            token = doc_re.escape(token)
+            for char in token:
+                if char.lower() != char.upper():
+                    new_token += '[' + char.lower() + char.upper() + ']'
+                    continue
+                new_token = new_token + char
+            if new_token:
+                tokens.append(new_token)
+
+        return rf'\b({"|".join(tokens)})\b'
 
 
 KEYWORDS = [
@@ -27,7 +44,7 @@ def sqleditor_tokens() -> list[tuple[str, Token]]:
         ("string1", Span('string', '"', '"', escape='\\')),
         ("string2", Span('string', "'", "'", escape='\\')),
         ("number", SingleToken('number', [r'\b[0-9]+(\.[0-9]*)*\b', r'\b\.[0-9]+\b'])),
-        ("keyword", Keywords('keyword', KEYWORDS)),
+        ("keyword", CaseInsensitiveKeywords('keyword', KEYWORDS)),
     ]
 
 
