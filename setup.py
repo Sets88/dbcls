5561ef79723013ed23c83720381a11dd8654e1f7
Maxim Nikitenko <mnikitenko@expertoption.com>
2024-05-22 10:40:29 +0300

    raise version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_requirements():
 
 setup(
     name='dbcls',
-    version='1.0.6',
+    version='1.0.7',
     license='BSD',
     author='Maksim Nikitenko',
     author_email='iam@sets88.com',
